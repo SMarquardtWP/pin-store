6e6863f6f5f7684debdf9a929790a7e9621f8358
Jonathan Yee <Jonathan.Yee.W@gmail.com>
2019-09-23 09:08:20 -0700

    0.1.1

--- client/src/views/Product.js
+++ client/src/views/Product.js
@@ -4,22 +4,23 @@ import { Link } from "react-router-dom";
 class Product extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props.location.state);
+    console.log("here", this.props.location.state);
   }
   render() {
+    const { product } = this.props.location.state;
     return (
       <div className="container">
         <hr />
         <div className="row p-0 m-0">
-          <div className="col-4 bg-success text-center">
-            <p>title</p>
-            <p>line</p>
-            <p>amount</p>
-            <p>description</p>
-            <button>add to cart?</button>
+          <div className="col-4 bg-success text-center pb-3 pt-3">
+            <p>{product.name}</p>
+            <hr />
+            <p>{product.amount}</p>
+            <p>{product.description_full}</p>
+            <button>add to cart</button>
           </div>
-          <div className="col-8 bg-primary">
-            <p>image here</p>
+          <div className="col-8 bg-primary d-flex justify-content-center align-items-center">
+            <img src={product.image_product} alt="" />
           </div>
         </div>
         <h1>Product</h1>
--- client/src/views/TemporaryDataBase.js
+++ client/src/views/TemporaryDataBase.js
@@ -8,8 +8,11 @@ const product1 = {
   product: {
     _id: 1,
     image_main: Photo,
+    image_product: Photo,
     name: "Cool Pin",
     description_short: "product description limited 64 characters",
+    description_full:
+      "products full description. Can be up to 270 characters so as to fit nicely in the description box.",
     price: "$10.00"
   },
   amount: 1
@@ -18,8 +21,11 @@ const product2 = {
   product: {
     _id: 2,
     image_main: Photo,
+    image_product: Photo,
     name: "Neat Pin",
     description_short: "product description limited 64 characters",
+    description_full:
+      "products full description. Can be up to 270 characters so as to fit nicely in the description box.",
     price: "$10.00"
   },
   amount: 2
@@ -28,8 +34,11 @@ const product3 = {
   product: {
     _id: 3,
     image_main: Photo,
+    image_product: Photo,
     name: "Dope Pin",
     description_short: "product description limited 64 characters",
+    description_full:
+      "products full description. Can be up to 270 characters so as to fit nicely in the description box.",
     price: "$10.00"
   },
   amount: 3
